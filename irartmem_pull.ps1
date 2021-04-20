342414b84cb13d546339c38b92c419590496e0c7
Sean Frank <78367642+seangfrank0474@users.noreply.github.com>
2021-04-19 20:27:23 -0400

    Add files via upload

---
+++ irartmem_pull.ps1
@@ -0,0 +1,13 @@
+﻿$url_source = @('https://github.com/seangfrank0474/IR_Artifact_Report_MemAquisition/blob/main/ir_artifact_rprt_mem_acq.ps1', 'https://github.com/seangfrank0474/IR_Artifact_Report_MemAquisition/blob/main/winpmem.exe', 'https://github.com/seangfrank0474/IR_Artifact_Report_MemAquisition/blob/main/7za.exe', 'https://github.com/seangfrank0474/IR_Artifact_Report_MemAquisition/blob/main/7za.dll', 'https://github.com/seangfrank0474/IR_Artifact_Report_MemAquisition/blob/main/7zxa.dll')
+Foreach ($url_line in $url_source) {
+    $file_split = $url_line -split "/"
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    foreach ($file in $file_split) {
+        switch -Regex ($file){
+            ".+\.(ps1|exe|dll)" { 
+                $file_dest = $env:SystemRoot + "\Temp\" + $file
+                Invoke-WebRequest -Uri $url_line -OutFile $file_dest
+            }
+        }
+    }
+}
