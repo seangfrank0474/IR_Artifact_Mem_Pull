a2ae6819d2a22b25a1fdf4a22bf63fcbe57abc21
Sean Frank <78367642+seangfrank0474@users.noreply.github.com>
2021-04-27 12:23:27 -0400

    Add files via upload

--- irartmem_pull.ps1
+++ irartmem_pull.ps1
@@ -1,13 +1,23 @@
-﻿$url_source = @('https://github.com/seangfrank0474/IR_Artifact_Report_MemAquisition/blob/main/ir_artifact_rprt_mem_acq.ps1', 'https://github.com/seangfrank0474/IR_Artifact_Report_MemAquisition/blob/main/winpmem.exe', 'https://github.com/seangfrank0474/IR_Artifact_Report_MemAquisition/blob/main/7za.exe', 'https://github.com/seangfrank0474/IR_Artifact_Report_MemAquisition/blob/main/7za.dll', 'https://github.com/seangfrank0474/IR_Artifact_Report_MemAquisition/blob/main/7zxa.dll')
+﻿$url_source = @("https://github.com/seangfrank0474/IR_Artifact_Report_MemAquisition/archive/refs/heads/main.zip")
+$pull_to_dir = ${env:ProgramFiles(x86)} + "\irts" 
+if (!(Test-Path -Path $pull_to_dir)){
+    New-Item -ItemType directory -Path $pull_to_dir
+    $screen_output = "[+] {0} IR Triage and Acquisition script path has been setup. Path: {1}" -f $(get-date -UFormat "%Y-%m-%dT%H:%M:%S"), $pull_to_dir
+    Write-Output $screen_output
+            }
+ 
 Foreach ($url_line in $url_source) {
     $file_split = $url_line -split "/"
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
     foreach ($file in $file_split) {
         switch -Regex ($file){
-            ".+\.(ps1|exe|dll)" { 
-                $file_dest = $env:SystemRoot + "\Temp\" + $file
+            ".+\.(ps1|exe|dll|zip)" { 
+                $file_dest = $pull_to_dir + "\" + $file
                 Invoke-WebRequest -Uri $url_line -OutFile $file_dest
             }
         }
     }
 }
+Expand-Archive -LiteralPath $file_dest -DestinationPath $pull_to_dir
+$screen_output = "[+] {0} IR Artifact Report and Memory Aquisition has been downloaded. Script Path: {1}" -f $(get-date -UFormat "%Y-%m-%dT%H:%M:%S"), $pull_to_dir
+Write-Output $screen_output
